c9a514f95d6def72cd70510e3c2971319d6ea59c
Henrique <hebaja@yahoo.com.br>
2024-07-18 01:24:44 -0300

    00 mains

---
+++ main00/ex00/main.c
@@ -0,0 +1,14 @@
+void ft_putchar(char c);
+
+int main() {
+    int i;
+    i = 32;
+
+    while (i <= 126)
+    {
+      ft_putchar(i);
+      i++;
+    }
+
+    return 0;
+}
---
+++ main00/ex04/main.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void ft_is_negative(int n);
+void ft_putchar(char c);
+
+int main()
+{
+  ft_putchar('0');
+  ft_is_negative(0);
+  ft_putchar(10);
+  ft_putchar('4');
+  ft_is_negative(4);
+  ft_putchar(10);
+  ft_putchar('-');
+  ft_putchar('1');
+  ft_is_negative(-1);
+
+  return (0);
+}
