c3fcdeba33efb6736e5f3076bf17692c926bf514
hebatist <hebaja@yahoo.com.br>
2024-07-21 16:51:05 -0300

    including unistd lib in some mains

--- main00/ex02/main.c
+++ main00/ex02/main.c
@@ -1,6 +1,11 @@
+#include <unistd.h>
+
 void ft_print_reverse_alphabet(void);
 
-void ft_puchar(char c);
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 int main()
 {
--- main00/ex03/main.c
+++ main00/ex03/main.c
@@ -1,6 +1,11 @@
+#include <unistd.h>
+
 void ft_print_numbers(void);
 
-void ft_putchar(10);
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 int main() 
 {
